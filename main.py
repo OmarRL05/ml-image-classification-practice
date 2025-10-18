06eac4dc79e075d0224d818348061e393529b259
Omar Reyes L. <relo2210@gmail.com>
2025-10-17 22:52:59 -0600

    feat: TestSplit the DataSet
    * TestSplit:
        + Divides the DataSet by 75% for training and 25% for testing.
        -- mnist.data contains the features of the DataSet.
        -- mnist.target contains the labels of the DataSet row.

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import f1_score
-
 mnist = fetch_openml('mnist_784',  as_frame=False)
 
 plt.figure(figsize=(20, 4))
-
 for index, img in zip(range(1, 9), mnist.data[:8]):
     plt.subplot(1, 8, index)
     plt.imshow(np.reshape(img, (28, 28)), cmap=plt.cm.gray)
     plt.title(f'Image No.{index} . ')
 plt.show()
 print(mnist.target[:8])
+
+X_train, X_test, y_train, y_test = train_test_split(mnist.data, mnist.target, test_size=0.25, random_state=42)
